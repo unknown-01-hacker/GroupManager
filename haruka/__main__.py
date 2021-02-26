61da3b48192456cdf05b41fda097fc16106f6413
unknown <67815810+unknown-01-hacker@users.noreply.github.com>
2021-02-26 21:07:11 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -24,22 +24,18 @@ PM_START = """Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group [manager bot](https://telegra.ph/file/948928fbfabd3607e3b73.jpg) and Anti-spam bot
 
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+My owner is @Raja_Kannada_Admin
 
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
+Special thanks to *Unknown person* for helping me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+         ©             .
 """
 
 
@@ -158,8 +154,10 @@ def send_start(bot, update):
     text = PM_START
 
     keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]]
-    keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
+    keyboard += [[InlineKeyboardButton(text="➕ Add Me ", callback_data="cntrl_panel_M"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
+    keyboard += [[InlineKeyboardButton(text="➕ Add Me ", callback_data="cntrl_panel_M"), 
+        InlineKeyboardButton(text="owner", callback_data="help_back")]]
 
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
